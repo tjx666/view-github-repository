05a3ece6f308adbf36db87216f02ce44bfc9acf1
YuTengjing <ytj2713151713@gmail.com>
2019-12-04 22:16:25 +0800

    :sparkles: feat: support open  external package github repository

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,76 @@
 /* eslint-disable no-else-return */
 import * as vscode from 'vscode';
+import axios from 'axios';
 import open from 'open';
+import _ from 'lodash';
 import { nodeNativeModules, getNodeNativeModuleDocumentURL } from './nodeNativeModules';
 
 const supportedLanguageIds = ['javascript', 'javascriptreact', 'typescriptreact', 'vue', 'typescript'];
 const importStatementRegexp = /require\(("|')([a-zA-Z0-9-]*?)("|')\)|import\s+.*?('|")([a-zA-Z0-9-]*?)('|")|export\s+.*?from\s+('|")([a-zA-Z0-9-]*?)('|")/g;
 
-const viewGithubRepository = (moduleName: string) => {
+const fetchNpmPackageRepository = async (moduleName: string): Promise<string | null> => {
+    // provided by https://api-docs.npms.io/
+    const packageInfoURL = 'https://api.npms.io/v2/package/:name';
+    let resp: any;
+    try {
+        resp = await axios.get(packageInfoURL.replace(':name', moduleName));
+    } catch (error) {
+        vscode.window.showErrorMessage(`Get module ${moduleName} info occur error, check your network!`);
+    }
+
+    const repositoryURL: string | undefined = _.get(resp, 'data.collected.metadata.links.repository');
+    if (repositoryURL) {
+        return repositoryURL;
+    }
+
+    return null;
+};
+
+const viewGithubRepository = async (moduleName: string) => {
     if (nodeNativeModules.includes(moduleName)) {
-        open(getNodeNativeModuleDocumentURL(moduleName));
+        await open(getNodeNativeModuleDocumentURL(moduleName));
     } else {
-        // ,,,
+        const repositoryURL = await fetchNpmPackageRepository(moduleName);
+        if (repositoryURL) {
+            open(repositoryURL);
+        } else {
+            vscode.window.showErrorMessage(`The module ${moduleName} doesn't seem to exist!`);
+        }
     }
 };
 
-const handleViewRepositoryOfEditor = () => {
+const extractModuleNames = (textContent: string): string[] | null => {
+    const importStatements = textContent.match(importStatementRegexp);
+    if (importStatements) {
+        return importStatements.map(importStatement => {
+            let matchedRegexp: RegExp | undefined;
+
+            if (importStatement.includes('require')) {
+                matchedRegexp = /require\(("|')([a-zA-Z0-9-]*?)("|')\)/;
+            } else if (importStatement.includes('import')) {
+                matchedRegexp = /import\s+.*?('|")([a-zA-Z0-9-]*?)('|")/;
+            } else {
+                matchedRegexp = /export\s+.*?from\s+('|")([a-zA-Z0-9-]*?)('|")/;
+            }
+
+            return importStatement.match(matchedRegexp)![2];
+        });
+    }
+    return null;
+};
+
+const handleViewRepositoryOfActiveEditor = () => {
     console.log('Execute command viewRepositoryOfEditor');
 
     const activeEditor = vscode.window.activeTextEditor;
     if (activeEditor && supportedLanguageIds.includes(activeEditor.document.languageId)) {
         const editorContent = activeEditor.document.getText();
-        const importStatements = editorContent.match(importStatementRegexp);
+        const moduleNames = extractModuleNames(editorContent);
 
-        if (importStatements) {
-            const packageNames = importStatements.map(importStatement => {
-                let matchedRegexp: RegExp | undefined;
-
-                if (importStatement.includes('require')) {
-                    matchedRegexp = /require\(("|')([a-zA-Z0-9-]*?)("|')\)/;
-                } else if (importStatement.includes('import')) {
-                    matchedRegexp = /import\s+.*?('|")([a-zA-Z0-9-]*?)('|")/;
-                } else {
-                    matchedRegexp = /export\s+.*?from\s+('|")([a-zA-Z0-9-]*?)('|")/;
-                }
-
-                return importStatement.match(matchedRegexp)![2];
-            });
-
-            if (packageNames.length === 1) {
-                viewGithubRepository(packageNames[0]);
-            } else {
-                // ...
-            }
+        if (moduleNames && moduleNames.length === 1) {
+            viewGithubRepository(moduleNames[0]);
+        } else {
+            // ...
         }
     }
 };
@@ -50,7 +79,10 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('Extension View GitHub Repository now active!');
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('viewGithubRepository.viewRepositoryOfEditor', handleViewRepositoryOfEditor)
+        vscode.commands.registerCommand(
+            'viewGithubRepository.viewRepositoryOfActiveEditor',
+            handleViewRepositoryOfActiveEditor
+        )
     );
 }
 
