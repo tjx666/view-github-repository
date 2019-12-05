48476849fbdd7f6d3a2ddb7f6715e227c9dabd4e
YuTengjing <ytj2713151713@gmail.com>
2019-12-05 16:49:30 +0800

    :fire: chore: remove useless code and comments

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -3,12 +3,7 @@ import Mocha from 'mocha';
 import glob from 'glob';
 
 export function run(): Promise<void> {
-    // Create the mocha test
-    const mocha = new Mocha({
-        ui: 'tdd',
-    });
-    mocha.useColors(true);
-
+    const mocha = new Mocha({ ui: 'tdd', useColors: true });
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((c, e) => {
@@ -17,11 +12,9 @@ export function run(): Promise<void> {
                 return e(err);
             }
 
-            // Add files to the test suite
             files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
             try {
-                // Run the mocha test
                 mocha.run(failures => {
                     if (failures > 0) {
                         e(new Error(`${failures} tests failed.`));
