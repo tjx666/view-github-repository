32ca2b095d574207a2e87528bc834e2019f521c9
YuTengjing <ytj2713151713@gmail.com>
2019-12-07 11:53:42 +0800

    :green_heart: fix: fix the ci build error

--- test/runTests.ts
+++ test/runTests.ts
@@ -1,12 +1,15 @@
 import { resolve } from 'path';
 import { runTests } from 'vscode-test';
+import { argv } from 'yargs';
 
 async function go() {
     const projectPath = resolve(__dirname, '../../');
     const extensionDevelopmentPath = projectPath;
     const extensionTestsPath = resolve(projectPath, './out/test/index.js');
     const testWorkspace = resolve(projectPath, './test-fixture');
-    const vscodeExecutablePath = resolve(projectPath, './.vscode-test/vscode-insiders/Code - Insiders.exe');
+    const vscodeExecutablePath = argv.production
+        ? undefined
+        : resolve(projectPath, './.vscode-test/vscode-insiders/Code - Insiders.exe');
 
     try {
         await runTests({
