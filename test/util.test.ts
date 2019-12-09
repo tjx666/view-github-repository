1b13e8b30f806e0e83c21d6ca1c2ac8a7529814b
YuTengjing <ytj2713151713@gmail.com>
2019-12-09 15:41:05 +0800

    :white_check_mark: test: add  test for getRootPath, getPackageNamesFromPackageJSON

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,7 +1,10 @@
-import { equal } from 'assert';
-import { fetchNpmPackageRepository } from '../src/util';
+import { resolve } from 'path';
+import assert, { equal } from 'assert';
+import fs from 'fs-extra';
+import { fetchNpmPackageRepository, getRootPath, getPackageNamesFromPackageJSON } from '../src/util';
 
 describe('#utils', () => {
+    const projectPath = resolve(__dirname, '../../');
     describe('#fetchNpmPackageRepository', function() {
         this.timeout(10 * 1000);
         it('normal package name: webpack', async () => {
@@ -11,15 +14,28 @@ describe('#utils', () => {
         });
 
         it('package name prefix with @xxx', async () => {
-            const babelRepositoryUrl = 'https://github.com/babel/babel';
+            const babelRepositoryUrl = 'https://github.com/babel/babel/tree/master/packages/babel-core';
             const responseUrl = await fetchNpmPackageRepository('@babel/core');
             equal(babelRepositoryUrl, responseUrl!);
         });
 
         it('return null when not exists package', async () => {
             const responseUrl: any = await fetchNpmPackageRepository('ytj');
-            console.error({ responseUrl });
             equal(null, responseUrl);
+        });
+    });
+
+    describe('#getRootPath', () => {
+        it('#should get the right project root path', () => {
+            equal(resolve(projectPath, './test-fixture'), getRootPath());
+        });
+    });
+
+    describe('#getPackageNamesFromPackageJSON', () => {
+        it('should get package names that not prefixed with @types/', async () => {
+            const jsonContent = await fs.readFile(resolve(getRootPath()!, 'package.json'));
+            const packageNames = getPackageNamesFromPackageJSON(jsonContent.toString());
+            assert(packageNames.includes('cross-env') && !packageNames.includes('@types/webpack'));
         });
     });
 });
